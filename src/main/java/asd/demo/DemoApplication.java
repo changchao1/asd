5cb3632e3294e57185b8f3967621484e1b9e859b
changchao1 <a984258488@163.com>
2022-03-02 01:13:32 +0800

    2

--- src/main/java/asd/demo/DemoApplication.java
+++ src/main/java/asd/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.printf("122");
         SpringApplication.run(DemoApplication.class, args);
     }
 
